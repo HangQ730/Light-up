e535bfa0a494a1d98acb9bb36451269448bc8df8
HangQi <hangqi0730@163.com>
2021-07-28 09:54:18 +0800

    Light-up2

--- Akari.java
+++ Akari.java
@@ -3,6 +3,10 @@
  *
  * @author Lyndon While 
  * @version 2021
+ * 
+ * Group Members: Hang Qi       22449507
+ *                Kate Gilbert  22968719
+ *                
  */
 import java.util.ArrayList; 
 
@@ -21,6 +25,54 @@ public class Akari
     public Akari(String filename)
     {
         // TODO 3
+        this.filename = filename;
+        FileIO fio = new FileIO(filename);
+        this.size = Integer.valueOf(fio.getLines().get(0));
+        this.board = new Space[this.size][this.size];
+        // Used to store the file contents to avoid reading disk files repeatedly.
+        String[] filetext = new String[6];
+        // Store the types of walls for future work.
+        Space[] spacetext = {Space.BLACK, Space.ZERO, Space.ONE, Space.TWO, 
+                             Space.THREE, Space.FOUR};
+        // Store the file contents in filetext.
+        for (int i=0;i<=5;i++){
+            filetext[i] = fio.getLines().get(i+1);
+        }
+        // Initialize board to EMPTY.
+        for (int i=0;i<size;i++){
+            for (int j=0;j<size;j++){
+                board[i][j] = Space.EMPTY;
+            }
+        }
+        // Add the puzzle contents according to the file.
+        for (int i=0;i<=5;i++){
+            for (int j=0;j<convertLocation(filetext[i]).size();j++){
+                int[] location = (int[])convertLocation(filetext[i]).get(j);
+                board[location[0]][location[1]] = spacetext[i];
+            }            
+        }
+        
+    }     
+    
+    /**
+     * This function is used to parse the file content. 
+     * Convert Strings to coordinates so that can be used directly.
+     */
+    private ArrayList convertLocation(String coordinate){
+        ArrayList<int[]> location = new ArrayList<int[]>();
+        String[] colist = coordinate.split(" ");
+        for (int i=0; i<colist.length; i++){
+            if (colist[i].length()>=2){
+                int x = parseIndex(colist[i].charAt(0));
+                for (int j=1; j<colist[i].length(); j++){
+                    int y = parseIndex(colist[i].charAt(j));
+                    int[] loactioni = {x,y};
+                    location.add(loactioni);
+                }
+            }
+        }
+        return location;
+
     }
     
     /**
@@ -37,7 +89,7 @@ public class Akari
     public String getFilename()
     {
         // TODO 1a
-        return null;
+        return filename;
     }
     
     /**
@@ -46,7 +98,7 @@ public class Akari
     public int getSize()
     {
         // TODO 1b
-        return -1;
+        return size;
     }
     
     /**
@@ -55,7 +107,7 @@ public class Akari
     public boolean isLegal(int k)
     {
         // TODO 5
-        return false; 
+        return k >= 0 && k < this.size; 
     }
     
     /**
@@ -64,7 +116,7 @@ public class Akari
     public boolean isLegal(int r, int c)
     {
         // TODO 6
-        return false; 
+        return isLegal(r) && isLegal(c);  
     }
     
     /**
@@ -74,7 +126,9 @@ public class Akari
     public Space getBoard(int r, int c)
     {
         // TODO 7
-        return null;
+        if (isLegal(r,c)) return board[r][c];
+        else throw new IllegalArgumentException();
+        
     }
     
     /**
@@ -86,7 +140,10 @@ public class Akari
     public static int parseIndex(char x)
     {
         // TODO 2
-        return -1;
+        if (Character.isDigit(x)) return (int)(x-48);
+        else 
+        if (Character.isUpperCase(x)) return (int)(x-55);
+        else throw new IllegalArgumentException();
     }
     
     /**
@@ -96,6 +153,27 @@ public class Akari
     public void leftClick(int r, int c)
     {
         // TODO 8
+        if (isLegal(r,c)){ 
+            if (board[r][c]==Space.EMPTY){
+                board[r][c]=Space.BULB;
+            }else if (board[r][c]==Space.BULB){
+                board[r][c]=Space.EMPTY;
+            }
+        }
+    }
+    
+    /**
+     * Performs a right click on the square at r,c if the indices are legal, o/w does nothing. 
+     * If r,c is mutable, then change to EMPTY.
+     */
+    public void rightClick(int r, int c)
+    {
+        // TODO 8
+        if (isLegal(r,c)){ 
+            if (Space.isMutable(board[r][c])){
+                board[r][c]=Space.EMPTY;
+            }
+        }
     }
     
     /**
@@ -104,6 +182,13 @@ public class Akari
     public void clear()
     {
         // TODO 4
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board.length;j++){
+                if (Space.isMutable(board[i][j])){
+                    board[i][j] = Space.EMPTY;
+                }
+            }
+        }
     }
     
     /**
@@ -113,7 +198,15 @@ public class Akari
     public int numberOfBulbs(int r, int c)
     {
         // TODO 14
-        return -1;
+        int n = 0;
+        if (isLegal(r,c)){
+            // Check the four adjacent positions.
+            if (isLegal(r+1,c) && board[r+1][c]==Space.BULB) n++;
+            if (isLegal(r-1,c) && board[r-1][c]==Space.BULB) n++;
+            if (isLegal(r,c+1) && board[r][c+1]==Space.BULB) n++;
+            if (isLegal(r,c-1) && board[r][c-1]==Space.BULB) n++;            
+        }else throw new IllegalArgumentException();
+        return n;
     }
     
     /**
@@ -123,6 +216,25 @@ public class Akari
     public boolean canSeeBulb(int r, int c)
     {
         // TODO 15
+        if (isLegal(r,c)){
+            // Check the four directions for any other bulbs
+            for (int i=1;i<this.size;i++){
+                if ((r+i)<this.size && !Space.isMutable(board[r+i][c])) break;
+                if ((r+i)<this.size && board[r+i][c]==Space.BULB) return true;
+            }
+            for (int i=1;i<this.size;i++){
+                if ((r-i)>=0 && !Space.isMutable(board[r-i][c])) break;
+                if ((r-i)>=0 && board[r-i][c]==Space.BULB) return true;
+            }
+            for (int i=1;i<this.size;i++){
+                if ((c+i)<this.size && !Space.isMutable(board[r][c+i])) break;
+                if ((c+i)<this.size && board[r][c+i]==Space.BULB) return true;
+            }
+            for (int i=1;i<this.size;i++){
+                if ((c-i)>=0 && !Space.isMutable(board[r][c-i])) break;
+                if ((c-i)>=0 && board[r][c-i]==Space.BULB) return true;
+            }
+        }else throw new IllegalArgumentException();
         return false;
     }
     
@@ -138,6 +250,33 @@ public class Akari
     public String isSolution()
     {
         // TODO 16
-        return null;
+        // Check each square for errors.
+        for (int i=0;i<size;i++){
+            for (int j=0;j<size;j++){
+                switch (board[i][j]){
+                case EMPTY:
+                    if (!canSeeBulb(i,j)) return "Unlit square at "+i+","+j;
+                    break;
+                case ZERO:
+                    if (numberOfBulbs(i,j)!=0) return "Broken number at "+i+","+j;
+                    break;
+                case ONE:
+                    if (numberOfBulbs(i,j)!=1) return "Broken number at "+i+","+j;
+                    break;
+                case TWO:
+                    if (numberOfBulbs(i,j)!=2) return "Broken number at "+i+","+j;
+                    break;
+                case THREE:
+                    if (numberOfBulbs(i,j)!=3) return "Broken number at "+i+","+j;
+                    break;
+                case FOUR:
+                    if (numberOfBulbs(i,j)!=4) return "Broken number at "+i+","+j;
+                    break;
+                case BULB:
+                    if (canSeeBulb(i,j)) return "Clashing bulb at "+i+","+j;
+                }
+            }
+        }
+        return "\u2713\u2713\u2713";
     }
 }
--- AkariViewer.java
+++ AkariViewer.java
@@ -3,14 +3,37 @@
  *
  * @author Lyndon While
  * @version 2021
+ * 
+ * Group Members: Hang Qi       22449507
+ *                Kate Gilbert  22968719
+ * 
  */
 import java.awt.*;
 import java.awt.event.*; 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AkariViewer implements MouseListener
 {    
-    private Akari puzzle;    // the internal representation of the puzzle
-    private SimpleCanvas sc; // the display window
+    private Akari puzzle; 
+    private SimpleCanvas sc; 
+    private int size;
+    private int gridsize = 50;
+    private int offset = gridsize/10;
+    
+    // All the colour settings. You can change the colours if you like.
+    private final Color bgColor  = Color.white;
+    private final Color boardLineColor  = Color.black;
+    private final Color wallColor  = Color.black;
+    private final Color gridColor  = Color.white;
+    private final Color litColor = Color.yellow;
+    private final Color numColor = Color.white;
+    private final Color fontColor = Color.black;
+    private final Color blueBulb = Color.blue;
+    private final Color redBulb = Color.red;
+    // Used to store the cross position.
+    private ArrayList cross = new ArrayList<String>();
 
     /**
      * Constructor for objects of class AkariViewer.
@@ -19,6 +42,15 @@ public class AkariViewer implements MouseListener
     public AkariViewer(Akari puzzle)
     {
         // TODO 10
+        this.puzzle = puzzle;
+        size = puzzle.getSize();
+        // Initialize canvas
+        sc = new SimpleCanvas("Akari Puzzle", size * gridsize + 50,  
+                              size * gridsize + 150, bgColor);
+        sc.addMouseListener(this);
+        displayPuzzle();
+        
+        
     }
     
     /**
@@ -44,7 +76,7 @@ public class AkariViewer implements MouseListener
     public Akari getPuzzle()
     {
         // TODO 9a
-        return null;
+        return this.puzzle;
     }
     
     /**
@@ -53,15 +85,110 @@ public class AkariViewer implements MouseListener
     public SimpleCanvas getCanvas()
     {
         // TODO 9b
-        return null;
+        return this.sc;
     }
     
+    /**
+     * Used to draw a single grid with board lines.
+     */    
+    private void drawBoard(int x, int y, Color c)
+    {
+        // Font settings
+        Font numfont = new Font("Calibri", Font.PLAIN, 20);
+        sc.drawRectangle(y * gridsize +25 , x * gridsize+25 , 
+                (y + 1) * gridsize+26, (x + 1) * gridsize+26, boardLineColor);
+        sc.drawRectangle(y * gridsize +26 , x * gridsize+26 , 
+                (y + 1) * gridsize+25, (x + 1) * gridsize+25, c);
+        // Add numbers to walls according to different wall types.
+        switch(puzzle.getBoard(x,y)){
+            case ZERO:
+                sc.setFont(numfont);
+                sc.drawString("0", (int) (23+gridsize * (y + 0.45)), 
+                            (int) (27+gridsize * (x + 0.6)), numColor);
+                break;
+            case ONE:
+                sc.setFont(numfont);
+                sc.drawString("1", (int) (23+gridsize * (y + 0.45)), 
+                            (int) (27+gridsize * (x + 0.6)), numColor);
+                break;
+            case TWO:
+                sc.setFont(numfont);
+                sc.drawString("2", (int) (23+gridsize * (y + 0.45)), 
+                            (int) (27+gridsize * (x + 0.6)), numColor);
+                break;
+            case THREE:
+                sc.setFont(numfont);
+                sc.drawString("3", (int) (23+gridsize * (y + 0.45)), 
+                            (int) (27+gridsize * (x + 0.6)), numColor);
+                break;
+            case FOUR:
+                sc.setFont(numfont);
+                sc.drawString("4", (int) (23+gridsize * (y + 0.45)), 
+                            (int) (27+gridsize * (x + 0.6)), numColor);
+                break;
+        
+        }
+    }
+        
     /**
      * Displays the initial puzzle; see the LMS page for a suggested layout. 
      */
     private void displayPuzzle()
     {
         // TODO 11
+        Font font = new Font("SansSerif", Font.PLAIN, 18);
+        Color c;
+        boolean isBulb = false;
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){   
+                if (puzzle.getBoard(i,j) == Space.EMPTY) c = gridColor;
+                else if (puzzle.getBoard(i,j) == Space.BULB){
+                    c=litColor;
+                    isBulb = true;                    
+                }
+                else c = wallColor; 
+                // Draw all the EMPTY squares and BULB squares.
+                drawBoard(i, j, c);
+                // If a square is BULB, then draw a bulb icon over it.
+                if (isBulb){
+                    sc.setFont(font);
+                    cross.remove(""+i+j);
+                    sc.drawString("\uD83D\uDCA1",j*gridsize+40, i*gridsize+57, blueBulb);
+                    isBulb = false;
+                }
+                // Draw the squares which are lit by a bulb.
+                if (puzzle.getBoard(i,j)==Space.EMPTY && puzzle.canSeeBulb(i,j)){           
+                    drawBoard(i, j, litColor);
+                }
+                // Change the bulb colour to red if there is a clashing bulb.
+                if (puzzle.getBoard(i,j)==Space.BULB && puzzle.canSeeBulb(i,j)){
+                    sc.drawString("\uD83D\uDCA1", j*gridsize+40, i*gridsize+57, redBulb);
+                }
+            }
+        }
+        // Check the cross and display.
+        for (Object i:cross){
+            boolean drawX = true; // Used to indicate whether or not to draw a cross.
+            String s = (String)i;
+            // Obtain the coordinate.
+            int x = Integer.parseInt(String.valueOf(s.charAt(0)));
+            int y = Integer.parseInt(String.valueOf(s.charAt(1)));
+            // If a bulb is placed over a cross, then delete that cross.
+            if (puzzle.getBoard(x,y)==Space.BULB){
+                cross.remove(i);
+                drawX = false;
+            }
+            // Draw cross icon.
+            if (drawX)
+                sc.setFont(font);
+                sc.drawString ("\u26CC", y*gridsize+40,x*gridsize+57,Color.red);
+                
+        }
+        sc.setFont(font);
+        sc.drawString ("SOLVED?", 65,size*gridsize+60, fontColor);
+        sc.drawString ("CLEAR", 265,size*gridsize+60, fontColor);
+        sc.drawRectangle(65,size*gridsize+70, 265,size*gridsize+120, bgColor);
+        
     }
     
     /**
@@ -71,10 +198,59 @@ public class AkariViewer implements MouseListener
     public void leftClick(int r, int c)
     {
         // TODO 12
+        puzzle.leftClick(r,c);
+        displayPuzzle();
+    }
+    
+    /**
+     * Performs a right click on the square at r,c if the indices are legal, o/w does nothing. 
+     * Updates both puzzle and the display. 
+     * If r,c is empty, a cross is placed; if it has a bulb, that bulb is removed and place a cross;
+     * if it has a cross, then remove it.
+     */
+    public void rightClick(int r, int c)
+    {
+        // TODO 12
+        puzzle.rightClick(r,c);
+        String cross_i = ""+r+c;
+        // If r,c is already in cross array, then remove it. If not, add it into cross.
+        boolean has = true;
+        for (int i=0;i<cross.size();i++){
+            if (cross.contains(cross_i)){
+                cross.remove(cross_i);
+                has = false;
+                break;
+            }
+        }
+        if (has && Space.isMutable(puzzle.getBoard(r,c))) cross.add(cross_i);
+        
+        displayPuzzle();
     }
     
     // TODO 13
-    public void mousePressed (MouseEvent e) {}
+    public void mousePressed (MouseEvent e) {
+        if (SwingUtilities.isLeftMouseButton(e)){
+            leftClick((e.getY()-25) / gridsize, (e.getX()-25) / gridsize);
+            // Perform the SOLVED? function.
+            if (e.getX()>55 && e.getX()<145 && 
+                e.getY()>size*gridsize+35 && e.getY()<size*gridsize+75){
+                    displayPuzzle();
+                    sc.drawString (puzzle.isSolution(), 65,size*gridsize+100, 
+                                    fontColor);                
+                }
+            // Perform the CLEAR funtion
+            if (e.getX()>255 && e.getX()<335 && 
+            e.getY()>size*gridsize+35 && e.getY()<size*gridsize+75){
+                puzzle.clear();
+                cross.clear();
+                displayPuzzle();
+            }
+        }else
+        if (SwingUtilities.isRightMouseButton(e)){
+            rightClick((e.getY()-25) / gridsize, (e.getX()-25) / gridsize);
+        }
+        
+    }
     public void mouseClicked (MouseEvent e) {}
     public void mouseReleased(MouseEvent e) {}
     public void mouseEntered (MouseEvent e) {}
